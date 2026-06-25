781d157756a23df0282347297011ce8f05debfaa
Ankit7n <ankitrawat7n@gmail.com>
2026-06-25 15:03:58 +0530

    origin updated

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://e-commerce-sable-psi.vercel.app/",
+    ],
     credentials: true,
   }),
 );
